3af5a03d3499b0ece9b6a41f099927de6573da1e
kateashely <valdezkateashley19@gmail.com>
2024-05-22 15:42:46 +0800

    Add project files.

---
+++ Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_VALDEZ_KATEASHLEY_BSIT_32E1.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<string> Moves { get; set; }
+        public List<string> Abilities { get; set; }
+    }
+}
