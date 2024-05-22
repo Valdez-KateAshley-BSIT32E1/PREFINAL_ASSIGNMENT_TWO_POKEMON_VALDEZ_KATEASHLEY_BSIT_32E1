a187df0d546cd16750684e66cebee944d85760c0
kateashely <valdezkateashley19@gmail.com>
2024-05-22 15:58:59 +0800

    Add Pokemon Controller

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_VALDEZ_KATEASHLEY_BSIT_32E1.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
