cf4297bdd6988be01b433de7b95ed66017ccbe62
kateashely <valdezkateashley19@gmail.com>
2024-05-22 15:57:07 +0800

    Add Pokemon model class

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -1,11 +1,6 @@
-﻿using System.Collections.Generic;
-
-namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_VALDEZ_KATEASHLEY_BSIT_32E1.Models
+﻿namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_VALDEZ_KATEASHLEY_BSIT_32E1.Models
 {
     public class Pokemon
     {
-        public string Name { get; set; }
-        public List<string> Moves { get; set; }
-        public List<string> Abilities { get; set; }
     }
 }
